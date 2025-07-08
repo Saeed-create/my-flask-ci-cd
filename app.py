8b93b835a84bc23803b97ff5208ef76827dc807a
deployuser <deployuser@ip-172-31-80-99.ec2.internal>
2025-07-08 10:58:08 +0000

    Update welcome message

--- app.py
+++ app.py
@@ -5,11 +5,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, CI/CD World from Flask on EC2!'
+    return 'Hello, CI/CD World from Flask on EC2! (Updated via CI/CD)' # This line was changed!
 
 @app.route('/test')
 def test_route():
     return 'This is a test route!'
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000) # Listen on all interfaces, port 5000    
+    app.run(host='0.0.0.0', port=5000)
